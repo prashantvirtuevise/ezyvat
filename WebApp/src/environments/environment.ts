7318cb6690ee295498dd45604e81329a021d9161
rohit luxor <rohitluxor@gmail.com>
2021-10-26 16:12:30 +0530

    updated env

--- WebApp/src/environments/environment.ts
+++ WebApp/src/environments/environment.ts
@@ -1,4 +1,4 @@
-  //LOCALHOST!!
+//LOCALHOST!!
 //    export const environment = {
 //   production: false,
 //   endPointApi: "http://localhost:51632",
@@ -15,25 +15,25 @@
 //      mapKey: "AIzaSyAXNRaWqYailuqorX_csBNZIKD0hGSxZ_A",
 //      getCoordLink: "https://maps.googleapis.com/maps/api/geocode/json?address="
 // };  
- 
+
 
 //TEST!
- export const environment = {
+export const environment = {
   production: false,
-  endPointApi: "http://localhost:51632",
-  endPointApiSupplier: "http://localhost:49331",
+  endPointApi: "https://test-eazyvat2-app-api.azurewebsites.net",
+  endPointApiSupplier: "https://test-eazyvat2-purchase-api.azurewebsites.net",
   endPointSignalR: "https://test-eazyvat2-app-api.azurewebsites.net",
   scanApi: "https://api.anyline.com/v1/scan",
   unknownPerson:
     "https://easyvatstore.blob.core.windows.net/maincontainer/unknown-person.png",
   scanLicense:
     "eyAiZGVidWdSZXBvcnRpbmciOiAib2ZmIiwgImltYWdlUmVwb3J0Q2FjaGluZyI6IGZhbHNlLCAibGljZW5zZUtleVZlcnNpb24iOiAyLCAibWFqb3JWZXJzaW9uIjogIjQiLCAibWF4RGF5c05vdFJlcG9ydGVkIjogMCwgInBpbmdSZXBvcnRpbmciOiB0cnVlLCAicGxhdGZvcm0iOiBbICJXZWIiIF0sICJzY29wZSI6IFsgIkFMTCIgXSwgInNob3dQb3BVcEFmdGVyRXhwaXJ5IjogdHJ1ZSwgInNob3dXYXRlcm1hcmsiOiBmYWxzZSwgInRvbGVyYW5jZURheXMiOiA1LCAidmFsaWQiOiAiMjAyMS0xMi0zMSIsICJ3ZWJJZGVudGlmaWVyIjogWyAiZWF6eXZhdC5jb20iIF0gfQpiZTJvQzFDT04wR2FSb3dld3FYQTNpNUFPalNKcVBQdXZ0TmU1dDVjRFZUOXROL3RLMnk1U1JWeTNaQzFpamE3Cjg1U0RqM3ZlWUl5TjdTQXdybld1M2lBaWFFYnFWMlp0ZkM0OGdBMkxVK0Fra0RoZ0FwbTRzVXVDZlpkTEw3Q0kKbTMycGIzemR6aENSNnJwbEozM1NERlVpV2M1UUd2ZGlEQmt3SDlGZ0ZyN3IyMlBqelM4S2xFMTh0aHVXbmpYQwpyWEdXUkx3SWhYMHIwU0gyQitjM1V1SkZFWTR0S0NHbVEvODFxeHphcWVpcFp2c1FBRkdFMmlSZ05XZnFhQnhDCjVPVWZTZTBBWDZXck4vWTRNeXU5MVYrT0dZWDl2Tmo2WjVCNXo4ZUVIVmxGblduOXNNQXVnV012eVhGWXphY2EKc0svT2xnWm56YXJoUXlpYXFwQi9wUT09Cg==",
-    tempToken :
-     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJDbGFpbUlkIjoiYTI1YWY0Y2MtYzI4Ni00NTc4LTE1MmEtMDhkNzgzM2EzM2Q1IiwiZXhwIjoxNjI4MDk2NzU2LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUxNjMyIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo1MTYzMiJ9.CJBUsZ7gGuLc6xSes5I8rv3w68ugsqAMcgrCXn9ko6A",
-     mapKey: " apiKey:'AIzaSyAXNRaWqYailuqorX_csBNZIKD0hGSxZ_A",
-     getCoordLink: "https://maps.googleapis.com/maps/api/geocode/json?address="
-};  
-  
+  tempToken:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJDbGFpbUlkIjoiYTI1YWY0Y2MtYzI4Ni00NTc4LTE1MmEtMDhkNzgzM2EzM2Q1IiwiZXhwIjoxNjI4MDk2NzU2LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjUxNjMyIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo1MTYzMiJ9.CJBUsZ7gGuLc6xSes5I8rv3w68ugsqAMcgrCXn9ko6A",
+  mapKey: " apiKey:'AIzaSyAXNRaWqYailuqorX_csBNZIKD0hGSxZ_A",
+  getCoordLink: "https://maps.googleapis.com/maps/api/geocode/json?address="
+};
+
 
 
 
